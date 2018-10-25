022b72c8d857da66ab3ca0fb7695e5ab9c46c333
Dmytro <dotjurin@gmail.com>
2018-10-25 23:27:28 +0300

    initial commit

--- gpurg-react/src/components/mainpage/mainPage.js
+++ gpurg-react/src/components/mainpage/mainPage.js
@@ -6,6 +6,7 @@ import FooterMessage from './footer';
 
 export default class MainPage extends React.Component {
     render() {
+        // comment for git
         return(
             <div>
                 <p>hi</p>
